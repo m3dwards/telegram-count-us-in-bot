7896f17f453216bdca60f5da3a8266fd04321ce8
Max Edwards <youwontforgetthis@gmail.com>
2021-02-18 23:15:59 +0000

    updated main.go

--- main.go
+++ main.go
@@ -229,7 +229,7 @@ func handleNewWatchParty(b *tb.Bot, filmName string, chatId int64, senderID int,
 	InOrOut := &tb.ReplyMarkup{}
 	btnIn := InOrOut.Data("I'm in!", "in", wpID)
 	btnOut := InOrOut.Data("I'm not in", "out", wpID)
-	btnInitiate := InOrOut.Data("Everyone is ready", "ready", wpID)
+	btnInitiate := InOrOut.Data("Attendance check complete \n Start countdown", "ready", wpID)
 	InOrOut.Inline(InOrOut.Row(btnIn, btnOut), InOrOut.Row(btnInitiate))
 
 	m, _ := b.Send(chat, "Nobody is in", InOrOut)
