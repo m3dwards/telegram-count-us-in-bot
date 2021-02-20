6a2119eaf44c8184eec362ab6446c4c38e7088d5
Max Edwards <youwontforgetthis@gmail.com>
2021-02-20 16:52:40 +0000

    updated main.go

--- main.go
+++ main.go
@@ -262,7 +262,7 @@ func getInOutMsg(wp *watchParty) string {
 }
 
 func getReadyMsg(wp *watchParty) string {
-	m := "*Get paused\\!* _Ready status will last for " + strconv.Itoa(countdownDuration) + " seconds_"
+	m := "*Get paused\\!* \n\n _Ready status will last for " + strconv.Itoa(countdownDuration) + " seconds_"
 	if len(wp.Viewers) == 0 {
 		return m
 	}
